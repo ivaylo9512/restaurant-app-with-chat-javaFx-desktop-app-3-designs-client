3d21cf31be11a5828b41c340826eb4d77d392f7c
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2019-02-22 03:17:46 +0200

    expand-order-pane logic changes

--- src/main/java/Animations/ExpandOrderPane.java
+++ src/main/java/Animations/ExpandOrderPane.java
@@ -1,113 +1,174 @@
 package Animations;
 
-
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.animation.TranslateTransition;
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.util.Duration;
 
 public class ExpandOrderPane {
-    private static Pane currentPane;
+    private static Pane currentOrder;
     private static Button button;
     private static Label label;
 
-    private static double width;
-    private static double height;
+    private static double orderWidth;
+    private static double orderHeight;
+    private static double buttonWidth;
+    private static double buttonHeight;
+    private static double labelWidth;
+
     private static double buttonX;
     private static double buttonY;
+
     private static double mouseY;
     private static double mouseX;
+
     private static double xButtonRation;
+    private static double initialOffsetX;
+    private static double initialMouseX;
 
-    public static Boolean buttonExpanded = false;
-    public static String currentStyle;
+    private static boolean action = false;
+    private static Boolean buttonExpanded = false;
 
-    public static void expandPane(Pane pane, MouseEvent event){
+    public static void expandPane(Pane order, Pane orderContainer, double scrolledAmount, MouseEvent event,
+                                  double translatePaneX, double translatePaneY, ScrollPane scrollPane){
+
+        if(action){
+            orderContainer.getChildren().add(order);
+            return;
+        }
         if(!buttonExpanded) {
-            currentPane = pane;
-            button = (Button) currentPane.getChildren().get(0);
-            label = (Label) currentPane.getChildren().get(1);
+            action = true;
+            currentOrder = order;
+            currentOrder.setLayoutX(translatePaneX - scrolledAmount);
+
+            button = (Button) currentOrder.getChildren().get(0);
+            label = (Label) currentOrder.getChildren().get(1);
 
             mouseX = event.getScreenX();
             mouseY = event.getScreenY();
             buttonX = button.getLayoutX();
             buttonY = button.getLayoutY();
+            initialOffsetX = event.getX() - translatePaneX + scrolledAmount;
+            System.out.println(initialOffsetX);
+            initialMouseX = event.getScreenX();
 
-            width = currentPane.getWidth();
-            height = currentPane.getHeight();
+            orderWidth = currentOrder.getWidth();
+            orderHeight = currentOrder.getHeight();
+            buttonWidth = button.getWidth();
+            buttonHeight = button.getHeight();
+            labelWidth = label.getWidth();
 
-            xButtonRation = currentPane.getPrefWidth() / (button.getLayoutX() + button.getPrefWidth() / 2);
+            xButtonRation = currentOrder.getPrefWidth() / (button.getLayoutX() + button.getPrefWidth() / 2);
         }
-        currentPane.setOnMousePressed(eventPress -> {
+
+        EventHandler panePress = (EventHandler<MouseEvent>) eventPress -> {
             mouseX = eventPress.getScreenX();
             mouseY = eventPress.getScreenY();
-        });
+        };
+        currentOrder.addEventFilter(MouseEvent.MOUSE_PRESSED, panePress);
 
-        currentPane.setOnMouseDragged(eventDrag -> {
-            currentPane.setTranslateX(currentPane.getTranslateX() + (eventDrag.getScreenX() - mouseX));
-            currentPane.setTranslateY(currentPane.getTranslateY() + (eventDrag.getScreenY() - mouseY));
+        EventHandler paneDrag = (EventHandler<MouseEvent>) eventDrag -> {
+            if(!buttonExpanded){
+                double fasterExpand = 1.8;
+                double expand = (initialMouseX - eventDrag.getScreenX()) * fasterExpand;
+                if(initialOffsetX < orderWidth / 2){
+                    if(expand < 0 ){
+                        expand = 0;
+                    }else{
+                        expand *= -1;
+                        currentOrder.setTranslateX(expand);
+                    }
+                }
+                System.out.println(expand);
+                if(expand >= 0){
+                    System.out.println(expand);
+                    label.setPrefWidth(labelWidth);
+                    order.setPrefWidth(orderWidth);
+                    order.setPrefHeight(orderHeight);
+                    button.setPrefWidth(buttonWidth);
+                    button.setPrefHeight(buttonHeight);
+                    button.setTranslateY(0);
+                    button.setTranslateX(0);
+                    order.setTranslateX(0);
+                }
+                if(orderWidth - expand > orderWidth) {
+                    button.setPrefWidth(buttonWidth - expand / 15);
+                    button.setPrefHeight(buttonWidth - expand / 15);
+                    currentOrder.setPrefWidth(orderWidth - expand);
+                    currentOrder.setPrefHeight(orderHeight - expand);
 
-            double expandX = mouseX - eventDrag.getScreenX();
-            double expandY = mouseY - eventDrag.getScreenY();
-            double expand;
+                    double translateButtonY = currentOrder.getPrefHeight() - button.getPrefHeight() - 10.5 - buttonY;
+                    double translateButtonX = (currentOrder.getPrefWidth() - button.getWidth()) / xButtonRation - buttonX;
 
-            if (expandY > 0) {
-                expand = Math.max(expandX, expandY);
-                expand += 1.3;
-            } else {
-                expand = Math.min(expandX, expandY);
-                expand -= 1.3;
+                    button.setTranslateX(translateButtonX);
+                    button.setTranslateY(translateButtonY);
+                    label.setPrefWidth(labelWidth - expand);
+
+                    double maxWidth = orderWidth * 4;
+                    if (currentOrder.getPrefWidth() > maxWidth) {
+                        buttonExpanded = true;
+                        mouseX = eventDrag.getScreenX();
+                        mouseY = eventDrag.getScreenY();
+                    }
+                }
+            }else {
+                currentOrder.setTranslateX((currentOrder.getTranslateX()) + (eventDrag.getScreenX() - mouseX));
+                currentOrder.setTranslateY((currentOrder.getTranslateY()) + (eventDrag.getScreenY() - mouseY));
+                mouseX = eventDrag.getScreenX();
+                mouseY = eventDrag.getScreenY();
             }
+        };
+        currentOrder.addEventFilter(MouseEvent.MOUSE_DRAGGED, paneDrag);
 
-            if(!buttonExpanded && currentPane.getPrefWidth() - expand > width ) {
+        EventHandler paneReleased = new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if(!buttonExpanded){
+                    currentOrder.removeEventFilter(MouseEvent.MOUSE_CLICKED, panePress);
+                    currentOrder.removeEventFilter(MouseEvent.MOUSE_DRAGGED, paneDrag);
+                    currentOrder.removeEventFilter(MouseEvent.MOUSE_RELEASED, this);
+                    TranslateTransition transitionPane = new TranslateTransition(Duration.millis(750),currentOrder);
+                    transitionPane.setToX(0);
+                    transitionPane.setToY(0);
+                    transitionPane.play();
 
-                button.setPrefWidth(button.getPrefWidth() - expand / 15);
-                button.setPrefHeight(button.getPrefHeight() - expand / 15);
-                currentPane.setPrefWidth(currentPane.getPrefWidth() - expand);
-                currentPane.setPrefHeight(currentPane.getPrefHeight() - expand);
+                    TranslateTransition transitionButton = new TranslateTransition(Duration.millis(750),button);
+                    transitionButton.setToX(0);
+                    transitionButton.setToY(0);
+                    transitionButton.play();
 
-                double translateY = currentPane.getPrefHeight() - button.getPrefHeight() - 10.5 - buttonY;
-                double translateX = (currentPane.getPrefWidth() - button.getWidth()) / xButtonRation - buttonX;
-                button.setTranslateX(translateX);
-                button.setTranslateY(translateY);
-                label.setPrefWidth(label.getPrefWidth() - expand);
+                    ResizeHeight heightPane = new ResizeHeight(Duration.millis(750),currentOrder,orderHeight);
+                    heightPane.play();
+                    ResizeWidth widthPane = new ResizeWidth(Duration.millis(750),currentOrder,orderWidth);
+                    widthPane.play();
 
-                if (currentPane.getPrefWidth() > 330) {
-                    buttonExpanded = true;
+                    ResizeHeight heightButton = new ResizeHeight(Duration.millis(750),button,buttonHeight);
+                    heightButton.play();
+                    ResizeWidth widthButton = new ResizeWidth(Duration.millis(750),button,buttonWidth);
+                    widthButton.play();
+
+                    ResizeWidth widthLabel = new ResizeWidth(Duration.millis(750),label,labelWidth);
+                    widthLabel.play();
+
+                    Timeline reAppendOrderInFlow = new Timeline(new KeyFrame(Duration.millis(750), actionEvent -> {
+                        currentOrder.setLayoutX(translatePaneX - orderContainer.getLayoutX());
+                        orderContainer.getChildren().add(currentOrder);
+                        currentOrder.setStyle("");
+                        scrollPane.setDisable(false);
+                        action = false;
+                    }));
+                    reAppendOrderInFlow.play();
                 }
             }
+        };
 
-            mouseX = eventDrag.getScreenX();
-            mouseY = eventDrag.getScreenY();
-        });
 
-        currentPane.setOnMouseReleased(eventRelease -> {
-            if(!buttonExpanded){
-                TranslateTransition transitionPane = new TranslateTransition(Duration.millis(500),currentPane);
-                transitionPane.setToX(0);
-                transitionPane.setToY(0);
-                transitionPane.play();
-
-                TranslateTransition transitionButton = new TranslateTransition(Duration.millis(500),button);
-                transitionButton.setToX(0);
-                transitionButton.setToY(0);
-                transitionButton.play();
-
-                ResizeHeight heightPane = new ResizeHeight(Duration.millis(500),currentPane,height);
-                heightPane.play();
-                ResizeWidth widthPane = new ResizeWidth(Duration.millis(500),currentPane,width);
-                widthPane.play();
-
-                ResizeHeight heightButton = new ResizeHeight(Duration.millis(500),button,28);
-                heightButton.play();
-                ResizeWidth widthButton = new ResizeWidth(Duration.millis(500),button,28);
-                widthButton.play();
-
-                ResizeWidth widthLabel = new ResizeWidth(Duration.millis(500),label,30);
-                widthLabel.play();
-            }
-        });
+        currentOrder.addEventFilter(MouseEvent.MOUSE_RELEASED, paneReleased);
     }
 }
