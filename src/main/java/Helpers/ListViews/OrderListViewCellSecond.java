e48609b5e2a3a2a17f822e9066c9090eda0feede
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2019-04-09 19:34:21 +0300

    small change - animate date labels on hover + selected cell

--- src/main/java/Helpers/ListViews/OrderListViewCellSecond.java
+++ src/main/java/Helpers/ListViews/OrderListViewCellSecond.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.util.Duration;
 import sample.LoggedThirdStyle;
 
@@ -15,17 +16,16 @@ import java.time.format.DateTimeFormatter;
 
 public class OrderListViewCellSecond extends ListCell<Order> {
     private DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
-
     private DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
 
     @FXML
-    private AnchorPane container;
+    private AnchorPane container, infoContainer;
+
+    @FXML
+    private Pane createdContainer, updatedContainer;
 
     @FXML
     private Label orderId, createdDate, createdTime, updatedDate, updatedTime;
-
-    @FXML
-    private AnchorPane infoContainer;
 
     private FXMLLoader mLLoader;
 
@@ -49,12 +49,37 @@ public class OrderListViewCellSecond extends ListCell<Order> {
                 }
             }
             container.setOnMouseClicked(event -> LoggedThirdStyle.controller.showOrder(order.getId()));
-
             createdDate.setText(dateFormatter.format(order.getCreated()));
             createdTime.setText(timeFormatter.format(order.getCreated()));
             updatedDate.setText(dateFormatter.format(order.getUpdated()));
             updatedTime.setText(timeFormatter.format(order.getUpdated()));
             orderId.setText(String.valueOf(order.getId()));
+
+            createdContainer.setOnMouseEntered(event -> {
+                createdDate.setText("created");
+                TranslateTransition translate = new TranslateTransition(Duration.millis(400), createdDate);
+                translate.setToX(30);
+                translate.play();
+            });
+            createdContainer.setOnMouseExited(event -> {
+                createdDate.setText(dateFormatter.format(order.getCreated()));
+                TranslateTransition translate = new TranslateTransition(Duration.millis(400), createdDate);
+                translate.setToX(0);
+                translate.play();
+            });
+            updatedContainer.setOnMouseEntered(event -> {
+                updatedDate.setText("updated");
+                TranslateTransition translate = new TranslateTransition(Duration.millis(400), updatedDate);
+                translate.setToX(30);
+                translate.play();
+            });
+            updatedContainer.setOnMouseExited(event -> {
+                updatedDate.setText(dateFormatter.format(order.getUpdated()));
+                TranslateTransition translate = new TranslateTransition(Duration.millis(400), updatedDate);
+                translate.setToX(0);
+                translate.play();
+            });
+
             setText(null);
             setGraphic(container);
         }
