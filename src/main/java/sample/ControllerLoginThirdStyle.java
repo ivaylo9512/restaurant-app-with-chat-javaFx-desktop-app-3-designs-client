1d51095258969df597f1c2df84cc01d00e29dd2c
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2019-04-10 22:13:20 +0300

    login third controller - login + register + updatefields + change scene

--- src/main/java/sample/ControllerLoginThirdStyle.java
+++ src/main/java/sample/ControllerLoginThirdStyle.java
@@ -20,7 +20,8 @@ import javafx.scene.layout.AnchorPane;
 import java.net.ConnectException;
 
 public class ControllerLoginThirdStyle {
-    @FXML TextField username, password, regUsername, regPassword, regRepeatPassword;
+    @FXML public TextField username, password, regUsername, regPassword, regRepeatPassword;
+    @FXML public AnchorPane root;
     private LoginService loginService;
     private RegisterService registerService;
 
@@ -38,10 +39,96 @@ public class ControllerLoginThirdStyle {
 
     @FXML
     public void login(Event event){
+        if(!KeyEvent.KEY_RELEASED.equals(event.getEventType()) || ((KeyEvent) event).getCode().equals(KeyCode.ENTER)) {
+            try {
+                username.setDisable(true);
+                password.setDisable(true);
+                root.setCursor(Cursor.WAIT);
 
+                loginService.start();
+            } catch (IllegalStateException e) {
+                System.out.println("request is executing");
+            }
+        }
+
+        loginService.setOnSucceeded(eventSuccess -> changeScene(loginService));
+        loginService.setOnFailed(eventFail -> updateError(loginService));
     }
     @FXML
     public void register(Event event) {
+        if(!KeyEvent.KEY_RELEASED.equals(event.getEventType()) || ((KeyEvent) event).getCode().equals(KeyCode.ENTER)) {
+            try {
+                regUsername.setDisable(true);
+                regPassword.setDisable(true);
+                regRepeatPassword.setDisable(true);
+                root.setCursor(Cursor.WAIT);
+
+                registerService.start();
+            } catch (IllegalStateException e) {
+                System.out.println("request is executing");
+            }
+        }
+
+        registerService.setOnSucceeded(eventSuccess -> changeScene(registerService));
+        registerService.setOnFailed(eventFail -> updateError(registerService));
+    }
+
+    private void updateError(Service service) {
+        username.setDisable(false);
+        password.setDisable(false);
+        regUsername.setDisable(false);
+        regPassword.setDisable(false);
+        regRepeatPassword.setDisable(false);
+        root.setCursor(Cursor.DEFAULT);
+
+        Alert alert = LoginSecondStyle.alert;
+        DialogPane dialog = alert.getDialogPane();
+        try{
+            dialog.setContentText(service.getException().getMessage());
+            throw service.getException();
+        }catch (ConnectException e){
+            dialog.setContentText("No connection to the server.");
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+        alert.showAndWait();
+
+        service.reset();
+    }
+
+    private void changeScene(Service service) {
+        User loggedUser = (User) service.getValue();
+        ServerRequests.loggedUserProperty.set(loggedUser);
+        ServerRequests.loggedUser = (User)service.getValue();
+
+        if(LoggedFirstStyle.stage != null){
+            LoggedSecondStyle.stage.show();
+            LoginSecondStyle.stage.close();
+        }else {
+            Platform.runLater(() -> {
+                try {
+                    LoggedSecondStyle.displayLoggedScene();
+                    LoginSecondStyle.stage.close();
+                } catch (Exception e) {
+                    DialogPane dialog = LoginSecondStyle.alert.getDialogPane();
+                    dialog.setContentText(e.getMessage());
+                    LoginSecondStyle.alert.showAndWait();
+                }
+            });
+        }
+        username.setDisable(false);
+        password.setDisable(false);
+        username.setText(null);
+        password.setText(null);
+        regUsername.setDisable(false);
+        regPassword.setDisable(false);
+        regRepeatPassword.setDisable(false);
+        regUsername.setText(null);
+        regPassword.setText(null);
+        regRepeatPassword.setText(null);
+        root.setCursor(Cursor.DEFAULT);
+
+        service.reset();
     }
 
 }
