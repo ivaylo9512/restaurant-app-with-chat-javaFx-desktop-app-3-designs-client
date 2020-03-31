ff329fb52cba692c6fe8722f6c4cd296e3c7e37b
ivailo aleksandrov <ivayloaleksandrov@yahoo.com>
2020-03-31 20:06:06 +0300

    small fix for TaskRequest

--- src/main/java/Helpers/TaskRequest.java
+++ src/main/java/Helpers/TaskRequest.java
@@ -1,5 +1,6 @@
 package Helpers;
 
+import Application.LoginManager;
 import javafx.concurrent.Task;
 import org.apache.http.HttpException;
 import org.apache.http.client.methods.HttpRequestBase;
@@ -14,18 +15,19 @@ import static Helpers.ServerRequests.mapper;
 public class TaskRequest<T> extends Task<T> {
 
     private Class<T> c;
+    HttpRequestBase request;
     public TaskRequest(Class<T> c) {
         this.c = c;
     }
 
     @Override
     protected T call() throws Exception {
+        request = createRequest(c);
         return executeTask();
     }
 
     private T executeTask() throws Exception{
         try {
-            HttpRequestBase request = createRequest(c);
             String content = executeRequest(request);
             if(content.equals("Success") || content.equals("Time out.")){
                 return null;
@@ -33,12 +35,18 @@ public class TaskRequest<T> extends Task<T> {
             return mapper.readValue(content, c);
 
         }catch (HttpException e) {
-            if(e.getMessage().equals("Jwt token has expired."))
-            loginManager.logout();
-            stageManager.showAlert("Session has expired.");
-
-            throw new HttpException(e.getMessage());
+            String message = e.getMessage();
+            if(message.equals("Jwt token has expired.")) {
+                loginManager.logout();
+                message = "Session has expired.";
+            }
+            stageManager.showAlert(message);
+            throw e;
         }catch (IOException e) {
+            if(stageManager.currentController instanceof LoginManager){
+                stageManager.showAlert("No connection to the server");
+                throw e;
+            }
             return executeTask();
         }
     }
